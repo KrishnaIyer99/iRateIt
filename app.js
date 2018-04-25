dc8ee6c433ddbc7af358af289a97371ab6f94c10
edanparker <33942519+edanparker@users.noreply.github.com>
2018-04-24 22:49:46 -0400

    Initial Commit
    Created basic server that can process a generic form submission and enter the data in a mongo collection.

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/iRateIt");
+const responseSchema = new mongoose.Schema({
+    session_id: String,
+    answer: Number,
+    answer1: Boolean,
+    answer2: Boolean,
+    answer3: Boolean,
+    answer4: Boolean,
+    answer5: Boolean,
+    dateTime: String
+});
+
+const User = mongoose.model("Responses", responseSchema);
+
+//middleware bodyparser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//input
+app.post("/login", (req, res) => {
+    res.sendFile(__dirname + "/login.html")
+  });
+
+app.post("/submit", (req, res) => {
+    var date = ({dateTime: (new Date())})
+    var data = Object.assign(req.body,date);
+    var myData = new User(data);
+    myData.save()
+      .then(item => {
+        res.send("Thanks")
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+    
+  });
+
+
+app.use("/",(req,res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(port,()=>{
+    console.log("Server running on port " + port)
+})
