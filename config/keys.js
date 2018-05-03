9a85f93159bbdeb9ef36bb593d71847e59dd0267
16kbf@queensu.ca <33009041+kaiferrall@users.noreply.github.com>
2018-05-03 22:23:56 +0200

    login, and signup handler. Bcrypt for password hashing and email validator. Also session tokens added.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://kai:kai@ds015878.mlab.com:15878/kaidevconnect"
+  mongoURI: "mongodb://kai:kai@ds015878.mlab.com:15878/kaidevconnect",
+  secretOrKey: "secret"
 };
